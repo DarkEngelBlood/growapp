5f782a822f8fe7d7d9a18476dc448b11cf22a6a1
Dario <darkengelblood@gmail.com>
2023-02-25 01:03:17 -0300

    subiendo los primeros archivos

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ItemListContainer from "../ItemListContainer/ItemListContainer";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <ItemListContainer />
+    </div>
+  );
+};
+
+export default Navbar;
